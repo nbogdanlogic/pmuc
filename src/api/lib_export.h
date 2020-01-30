a1a363f2336151f2f973c2278b99bc494363ebd4
Николай Богдан <n.bogdan@logic-way.com>
2020-01-30 13:00:02 +0300

    Hot fix for compile on linux

--- src/api/lib_export.h
+++ src/api/lib_export.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#ifndef __linux__
-#if defined(PMUC_EXPORTS)
-#define DLL_PMUC_EXPORT __declspec(dllexport)
-#else  // !BUILDING_DLL
-#define DLL_PMUC_EXPORT __declspec(dllimport)
-#endif  // BUILDING_DLL
-#else
-#define DLL_pmuc_EXPORT
-#endif  // __linux__
+#if defined(_WIN32)
+	#if defined(PMUC_EXPORTS)
+		#define DLL_PMUC_EXPORT __declspec(dllexport)
+	#else  // !BUILDING_DLL
+		#define DLL_PMUC_EXPORT __declspec(dllimport)
+	#endif  // BUILDING_DLL
+#else // / UNIX /
+	#define DLL_PMUC_EXPORT
+#endif // 
