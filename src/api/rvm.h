1ad761bbe90236f1e4b53611c644987ee74660f1
Николай Богдан <n.bogdan@logic-way.com>
2020-02-04 10:06:22 +0300

    Physical remove file

--- src/api/rvm.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "lib_export.h"
-#include "rvmcolorhelper.h"
-#include "rvmmeshhelper.h"
-#include "rvmparser.h"
-#include "rvmprimitive.h"
-#include "rvmreader.h"
-#include "vector3f.h"
